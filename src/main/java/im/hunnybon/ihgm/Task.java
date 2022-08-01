547a7ad323cb378d80844e05241cfebf1c81c208
labaredaf <105746616+labaredaf@users.noreply.github.com>
2022-08-01 19:25:39 -0300

    Fix day count skyrocketing when no players are online

--- src/main/java/im/hunnybon/ihgm/Task.java
+++ src/main/java/im/hunnybon/ihgm/Task.java
@@ -8,8 +8,8 @@ public class Task extends BukkitRunnable {
     @Override
     public void run(){
         if (Bukkit.getOnlinePlayers().size() == 0) {
-            if (!Boolean.FALSE.equals(Bukkit.getWorlds().get(0).getGameRuleValue(GameRule.DO_DAYLIGHT_CYCLE))) {
-                Bukkit.getWorlds().get(0).setTime(Bukkit.getWorlds().get(0).getTime() - 1);
+            if (Boolean.TRUE.equals(Bukkit.getWorlds().get(0).getGameRuleValue(GameRule.DO_DAYLIGHT_CYCLE))) {
+                Bukkit.getWorlds().get(0).setFullTime(Bukkit.getWorlds().get(0).getFullTime() - 1);
             }
         }
     }
